8f94f601f41c891fbb75cbecf2486748a056856c
Aleksandr Klenin <59577486+Sol-max@users.noreply.github.com>
2023-08-31 19:02:21 +0000

    test passed

--- src/app.py
+++ src/app.py
@@ -25,18 +25,45 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# GET all members
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def handle_members():
+        members = jackson_family.get_all_members()
+        
+        return jsonify(members), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+# POST a new member
+@app.route('/member', methods=['POST'])
+def handle_member_post():
+    try:
+        data = request.json
+        if 'first_name' not in data or 'age' not in data or 'lucky_numbers' not in data:
+            return jsonify({"message": "Incomplete request. Missing fields"}), 400
+        
+        new_member = jackson_family.add_member(data)
+        return jsonify(new_member), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
 
-    return jsonify(response_body), 200
+# GET, POST, and DELETE a specific member
+@app.route('/member/<int:member_id>', methods=['GET', 'DELETE'])
+def handle_member(member_id):
+    try:
+        if request.method == 'GET': 
+            member = jackson_family.get_member(member_id)
+            if member is None:
+                return jsonify({"message": "Member not found"}), 404
+            return jsonify(member), 200
+
+        if request.method == 'DELETE':
+            deleted = jackson_family.delete_member(member_id)
+            if deleted:
+                return jsonify({"done": True}), 200 
+            else:
+                return jsonify({"message": "Member not found"}), 404
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,26 +11,42 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [
+            {'id': self._generateId(), 'first_name': 'John', 'last_name': 'Jackson', 'age': 33, 'lucky_numbers': [7, 13, 22]},
+            {'id': self._generateId(), 'first_name': 'Jane', 'last_name': 'Jackson', 'age': 35, 'lucky_numbers': [10, 14, 3]},
+            {'id': self._generateId(), 'first_name': 'Jimmy', 'last_name': 'Jackson', 'age': 5, 'lucky_numbers': [1]}
+        ]
 
-        # example list of members
-        self._members = []
-
-    # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member['last_name'] = self.last_name
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        member_to_delete = [member for member in self._members if member['id'] == id]
+        if member_to_delete:
+            self._members.remove(member_to_delete[0])
+            return member_to_delete[0]
+        else:
+            return None
+
+    def update_member(self, id, new_info):
+        member_to_update = [member for member in self._members if member['id'] == id]
+        if member_to_update:
+            member_to_update[0].update(new_info)
+            return member_to_update[0]
+        else:
+            return None
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        member_to_get = [member for member in self._members if member['id'] == id]
+        return member_to_get[0] if member_to_get else None
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+# Initialize the class
+jackson_family = FamilyStructure('Jackson')
